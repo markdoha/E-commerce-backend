fb41499a69895ca3cf2219df0ba6c54a84cea196
mark mamdouh <73199677+markdoha@users.noreply.github.com>
2022-07-08 19:27:04 +0200

    password hashing and .env

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,6 +1,14 @@
 // importing client model
 const client = require("../models/client.model")
 
+// bycrpt 
+let bycrpt = require("bcryptjs");
+
+//enviroment variables
+let env = require("dotenv").config()
+
+let peaper = process.env.BYCRPT
+let sault = process.env.SAULT
 
 let printALLClients = async (req, res) => {
     try {
@@ -16,9 +24,10 @@ let addClient = async (req, res) => {
     try {
             const newUser = new client(req.body)
             if(!newUser.name || !newUser.userName || !newUser.email || !newUser.password || !newUser.creditCard || !newUser.creditCardExp || !newUser.creditCardCcv){
-                res.json( { success: "False // ENTER VALID DATA PLZZZZZZZ" } )
+                res.json( { success: "False // ENTER VALID DATA PLZZZZZZZ" } );
             }
             else{
+            newUser.password = bycrpt.hashSync(newUser.password, peaper, sault);
             await newUser.save();
                 res.status(201).json({success: "true"})
             }
